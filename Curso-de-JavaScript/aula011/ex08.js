76f0aa09c1c79c0cb2e09caee573dca8ef462551
Guilherme Barreto <barretogui90@gmail.com>
2020-11-05 14:01:58 -0300

    aula011

---
+++ Curso-de-JavaScript/aula011/ex08.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade do seu carro  ${vel}`)
+if (vel > 60) { //condição simples!
+    console.log('Você ultrapassou a velocidade permitida. Multado!!!')
+}
+console.log('Digija usando sempre cinto de segurança')
